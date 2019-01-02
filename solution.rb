118f086b0210b9adf5fb5cbc51f36a505b6dcf36
Sebastian Echeverri <sebastianecheverri900@gmail.com>
2019-01-02 15:26:28 -0500

    reto 2 done

--- solution.rb
+++ solution.rb
@@ -1,9 +1,6 @@
 require 'sinatra'
 
 get '/:nombre' do
-  unless params[:nombre] != '' && params[:nombre]
-    "<h1>Hola desconocido!</h1>"
-  else
     "<h1>Hola #{params[:nombre].capitalize}!</h1>"
   end
 end
